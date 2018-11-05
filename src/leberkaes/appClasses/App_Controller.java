c82311649880fe6724f40606053cc930c85de899
Dominik Holliger <holliger-it@192.168.22.115>
2018-11-05 21:37:57 +0100

    added eventlistener for gameserver menue

--- src/leberkaes/appClasses/App_Controller.java
+++ src/leberkaes/appClasses/App_Controller.java
@@ -19,8 +19,22 @@ public class App_Controller extends Controller<App_Model, App_View> {
 
     public App_Controller(App_Model model, App_View view) {
         super(model, view);
+
+        // register ourselves to listen for our menu item clicks
+        view.GameServerStart.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent t) {
+            	serviceLocator.getLogger().info("GameServer - start requested");
+            }
+        });
+        view.GameServerSettings.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent t) {
+            	 serviceLocator.getLogger().info("GameServer - settings clicked");
+            }
+        });
         
-     // register ourselves to listen for button clicks
+        
+        
+        // register ourselves to listen for button clicks
         view.btnClick.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
