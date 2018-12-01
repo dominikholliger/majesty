4ec4f5c6a9f5c0eac8a993e56a2e41e51f0b915c
Dominik Holliger <holliger-it@192.168.22.115>
2018-12-01 09:23:19 +0100

    ssss

---
+++ src/leberkaes/gameClient/dummyFXMLControllerGameBoard.java
@@ -0,0 +1,20 @@
+package leberkaes.gameClient;
+
+
+public class dummyFXMLControllerGameBoard {
+	private static GameClient_Controller _GMvcCtrl;
+	
+		public static GameClient_Controller get_GMvcCtrl() {
+			return _GMvcCtrl;
+		}
+		@SuppressWarnings("static-access")
+		public void set_GMvcCtrl(GameClient_Controller _GMvcCtrl) {
+			this._GMvcCtrl = _GMvcCtrl;
+		}
+		
+		
+
+
+	
+
+}
---
+++ src/leberkaes/gameServer/dummyFXMLControllerNewGame.java
@@ -0,0 +1,38 @@
+package leberkaes.gameServer;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import leberkaes.gameClient.GameClient_Controller;
+import leberkaes.gameClient.GameClient_View;
+
+public class dummyFXMLControllerNewGame {
+
+	
+		private GameServer_Controller _NMvcCtrl;
+
+		public GameServer_Controller get_NMvcCtrl() {
+			return _NMvcCtrl;
+		}
+		
+	
+		public void set_NMvcCtrl(GameServer_Controller _NMvcCtrl) {
+			this._NMvcCtrl = _NMvcCtrl;
+		}
+
+		@FXML protected void handleStartGameClicked(ActionEvent event) throws Exception {
+			// get_NMvcCtrl().goToGameBoard();
+			
+		}
+		
+		@FXML protected void handleTwoPlayersClicked(ActionEvent event) throws Exception {
+			// TODO
+		}
+		
+		@FXML protected void handleFourPlayersClicked(ActionEvent event) throws Exception {
+			// TODO
+		}
+	}
+
+	
+
+
