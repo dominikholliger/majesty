3e2071e15cd6ecf556233b56544e7c11d23ede53
Dominik Holliger <info@holliger-it.ch>
2018-12-04 17:36:03 +0100

    HomeButton mit Arsch-Beispiel

--- src/leberkaes/appClasses/dummyFXMLControllerHome.java
+++ src/leberkaes/appClasses/dummyFXMLControllerHome.java
@@ -2,30 +2,31 @@ package leberkaes.appClasses;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class dummyFXMLControllerHome {
-	
-	/** 
+
+	/**
 	 * Anmerkung D.Holliger:
 	 * Vorsicht: FXML Controller versus MVC Controller!
 	 * Um den generierten Kontroller von FXML an das MVC Framework von Brad anzudocken habe ich einige Anpassungen vorgenommen Ausgangslage:
 	 * - Der FXMLoader lädt die fxml bei der Instanzierung des Views des MVC Modelles. Zu diesem Zeitpunkt existiert noch kein MVC Controller.
 	 * - Der FMXLoader will den angegeben Kontrollerklasse instanzieren und erwartert eine statische Klasse ohne Konstruktor
-	 * 
+	 *
 	 * Implementation:
 	 * - Im FXML keine Kontroller Klasse angeben
 	 * - Kontrollerklasse (generierte Datei) von FXML mit dem Getter / Setter für ein APP_Controller Objekt (Controller MVC Brad) erstellen
 	 * - Im View des MVC Brad den FXML Loader setzen, inkl. definition der ControllerKlasse -> Diese Dumy-Kontroller Klasse angeben (welche vom Scene Builder generiert wird).
 	 * - Im View per Lazy Loading eine Instanz des Dummy-Controllers dem FXML Loader übergeben.
-	 * 
+	 *
 	 * Damit arbeiten:
 	 * - In diesem Dummy-Kontroller können nun per this._MvcCtrl-> Methoden auf Methoden im MVC Kontroller von Brad gemappt werden.
-	 * 
+	 *
 	 * Ausbau:
 	 * - Theoretisch könnte man dies noch mit einer Abstrakten Klasse vereinfachen, aber ich glaube wir lassen es aus Zeitgründen.
 	 */
-	
+
 	private App_Controller _MvcCtrl;
 	public App_Controller get_MvcCtrl() {
 		return _MvcCtrl;
@@ -34,10 +35,14 @@ public class dummyFXMLControllerHome {
 		this._MvcCtrl = _MvcCtrl;
 	}
 
+	@FXML
+	private Button newGame;
+
 
 	@FXML protected void handleNewGameButtonClicked(ActionEvent event) throws Exception {
 		// Server Prozess starten
 		get_MvcCtrl().startNewServerProcess();
+		newGame.setText("Arsch");
 	}
 	@FXML protected void handleSettingsButtonClicked(ActionEvent event) throws Exception{
 		// Einstellungen öffnen
@@ -46,18 +51,18 @@ public class dummyFXMLControllerHome {
 	@FXML protected void handleHighscoreButtonClicked(ActionEvent event) throws Exception{
 		// Highscore-Fenster oeffnet sich, nichts wird uebergeben
 		get_MvcCtrl().openHighscoreWindow();
-		
+
 	}
 	@FXML protected void handleEnterGameButtonClicked(ActionEvent event) throws Exception{
 		// Client Prozess starten
 		get_MvcCtrl().startNewClientProcess();
 	}
-	
+
 	@FXML protected void handleBackClicked(ActionEvent event) throws Exception{
 		// Client Prozess starten
 		get_MvcCtrl().showHomeWindow();
 	}
-	
-	
-	
+
+
+
 }
