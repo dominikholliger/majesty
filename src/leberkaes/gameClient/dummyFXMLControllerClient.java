f6c81348c24e67de134398b5d411cd03b818253d
Sebrina <Sebrina@192.168.0.42>
2018-12-09 11:40:15 +0100

    Updated GUI once more

--- src/leberkaes/gameClient/dummyFXMLControllerClient.java
+++ src/leberkaes/gameClient/dummyFXMLControllerClient.java
@@ -54,6 +54,7 @@ public class dummyFXMLControllerClient {
 	@FXML public TextField txtGamePort;
 	@FXML public TextField txtIpAddress;
 	@FXML public Button btnConnect;
+	@FXML public Text labelRoundCount;
 	
 	// Deck Cards
 	@FXML public ImageView openDeck5;
@@ -204,6 +205,9 @@ public class dummyFXMLControllerClient {
 			this.openDeck4.setImage(new Image(g.getOpenDeck()[4].getFrontImgURL()));
 			this.openDeck5.setImage(new Image(g.getOpenDeck()[5].getFrontImgURL()));
 			this.deckBack.setImage(new Image(g.getDeck().peek().getBackImgURL()));
+			
+		// set correct RoundCount
+			this.labelRoundCount.setText(String.valueOf(g.getRoundCount()));
 		
 			// get Number of Players in Game
 			int i = g.getPlayers().size();
